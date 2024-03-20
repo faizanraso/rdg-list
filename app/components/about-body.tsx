ddc0e39efcf561281c2a9011d6246886413b7014
Faizan Rasool <faizanraso@outlook.com>
2024-03-20 18:47:46 -0400

    Sections added

--- app/components/about-body.tsx
+++ app/components/about-body.tsx
@@ -2,21 +2,57 @@ import React from "react";
 
 export default function AboutBody() {
   return (
-    <main className="py-12">
-      <div>
-        <div className="py-1.5">
-          <p className="text-neutral-200 text-sm font-medium tracking-wide leading-relaxed">
-            rdg-list
+    <main className="py-12 tracking-wide">
+      <div className="text-justify py-2.5">
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-200 text-sm">
+            (Reading List) rdg-list
           </p>
         </div>
-        <div className="py-1.5">
-          <p className="text-neutral-400 text-sm tracking-wide font-light text-justify leading-relaxed">
-            (Reading) <span>rdg-list</span> is a simple web app that serves as a
-            home for all of your internet finds. Whether it's an article you
-            want to finish reading later, a regex tutorial you plan on coming
-            back to <span className="italic underline">one day</span>, or
-            anything in between. rdg-list makes it easy to store and group your
-            links in one place.
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-400 text-sm font-light">
+            A simple web app that serves as a home for all of your internet
+            finds. Whether it's an article you want to finish reading later, a
+            regex tutorial you plan on coming back to one day, or anything in
+            between. rdg-list makes it easy to store and group your links in one
+            place.
+          </p>
+        </div>
+      </div>
+      <div className="text-justify py-2.5">
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-200 text-sm">
+            Inspiration
+          </p>
+        </div>
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-400 text-sm font-light">
+            This app is heavily inspired by{" "}
+            <a href={"https://bmrks.com/"} className="underline italic">
+              bmrks.com
+            </a>
+            . I've pretty much borrowed the whole concept and style from it. But
+            hey, I'm not just copying - I'm throwing in some extra features that
+            I find useful.
+          </p>
+        </div>
+      </div>
+      <div className="text-justify py-2.5">
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-200 text-sm">Usage</p>
+        </div>
+        <div className="py-1">
+          <p className="leading-relaxed text-neutral-400 text-sm font-light">
+            This app is free to use. To get started, create an account using the
+            "login" link above. Additionally,{" "}
+            <span className="font-medium">rdg-list</span> is{" "}
+            <a
+              href="https://github.com/faizanraso/rdg-list"
+              className="underline"
+            >
+              open source
+            </a>
+            .
           </p>
         </div>
       </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,18 +3,15 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row justify-between">
-      <div>
-        <p className="text-xs font-medium text-neutral-200">rdg-list</p>
-      </div>
-      <div>
-        <Link
-          to="/login"
-          className="text-xs underline text-neutral-400 hover:text-neutral-300 transition duration-150"
-        >
-          Login
-        </Link>
-      </div>
+    <nav className="flex flex-row items-center justify-between">
+      <p className="text-xs font-medium text-neutral-200">rdg-list</p>
+
+      <Link
+        to="/login"
+        className="text-xs underline text-neutral-400 hover:text-neutral-300 transition duration-150"
+      >
+        Login
+      </Link>
     </nav>
   );
 }
