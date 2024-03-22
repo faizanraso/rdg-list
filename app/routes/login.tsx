02d3aa246103fa985a31794a720138719a79bc19
Faizan Rasool <faizanraso@outlook.com>
2024-03-22 18:45:57 -0400

    meta data

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { MetaFunction } from "@remix-run/node";
+
 import NavBar from "~/components/login/navbar";
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: "rdg-list - login",
+    },
+    { name: "Login Page", content: "Login to rdg-list." },
+  ];
+};
 
 export default function LoginPage() {
   return (
