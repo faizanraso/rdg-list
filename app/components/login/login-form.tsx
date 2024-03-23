ebe73e0b0d08f436a15c21ed73f8d79c219ea78a
Faizan Rasool <faizanraso@outlook.com>
2024-03-23 18:29:45 -0400

    deleted extra file

--- app/components/login/login-form.tsx
+++ app/components/login/login-form.tsx
@@ -4,13 +4,18 @@ export default function LoginForm() {
   return (
     <main className="py-20 items-center justify-center flex flex-col">
       <div className="max-w-96 w-full">
-        <p className="text-lg text-neutral-200 leading-relaxed">Login</p>
-        <p className="text-sm text-neutral-400 leading-relaxed">
-          Don't have an account?{" "}
-          <Link to={"/register"} className="underline">
-            Register
-          </Link>
-        </p>
+        <div className="border-b border-neutral-800 py-4">
+          <p className="text-lg text-neutral-200 leading-relaxed">Login</p>
+          <p className="text-sm text-neutral-400 leading-relaxed">
+            Don't have an account?{" "}
+            <Link to={"/register"} className="underline">
+              Register
+            </Link>
+          </p>
+        </div>
+        <div>
+          
+        </div>
       </div>
     </main>
   );
